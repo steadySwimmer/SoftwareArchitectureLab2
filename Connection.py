ae97a2464d5add3d58f1c6ae6d88b875194fa5d2
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2017-05-29 21:36:40 +0300

    Establish connection

---
+++ Connection.py
@@ -0,0 +1,15 @@
+""" Module that provides conn object for the model """
+
+import sqlobject
+
+conn = None
+
+def establish_connection(save_type_key):
+    """ Connect to the database """
+    if save_type_key == "mysql":
+        #conn = sqlobject.mysql.builder()()
+        pass
+    elif save_type_key == "sqll":
+        conn = sqlobject.sqlite.builder()("test.db")
+    elif save_type_key == "psql":
+        conn = sqlobject == sqlobject.postgres.builder()
