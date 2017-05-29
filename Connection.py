c68e1eaa511fac932c135d88ecdbdb894e697e93
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2017-05-29 22:20:57 +0300

    Fix connection

--- Connection.py
+++ Connection.py
@@ -1,15 +1,21 @@
 """ Module that provides conn object for the model """
 
+import os
 import sqlobject
 
 conn = None
 
 def establish_connection(save_type_key):
     """ Connect to the database """
+    db_filename = os.path.abspath("mdata.db")
     if save_type_key == "mysql":
-        #conn = sqlobject.mysql.builder()()
-        pass
+        connection_str = "mysql://host" + db_filename
+        conn = sqlobject.connectionForURI(connection_str)
+
     elif save_type_key == "sqll":
-        conn = sqlobject.sqlite.builder()("test.db")
+        connection_str = "sqlite:" + db_filename
+        conn = sqlobject.connectionForURI(connection_str)
+
     elif save_type_key == "psql":
-        conn = sqlobject == sqlobject.postgres.builder()
+        connection_str = "postgres://host" + db_filename
+        conn = sqlobject.connectionForURI(connection_str)
